0ae24670fc08e84ed0da1bf0b1ea7f29b11a39bc
Ochomozone <112177400+Ochomozone@users.noreply.github.com>
2024-03-08 08:24:00 +0300

    token sent to backend

--- src/AppBackup.js
+++ src/AppBackup.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 import NavigationBar from './components/NavBar';
 import Home from './components/Home';
@@ -14,56 +15,57 @@ import History from './components/History';
 const baseUrl = 'http://localhost:4001';
 
 function App() {
+  // const [user, setUser] = useState(null);
   const [profile, setProfile] = useState(null);
   const navigate = useNavigate();
 
+  // Function to handle user login
+  const handleLogin = () => {
+    login();
+  };
+
+  // Function to handle user logout
   const handleLogout = () => {
-    // Implement your logout logic
+    googleLogout();
+    setProfile(null);
+    navigate('/');
+    sessionStorage.removeItem('profile');
   };
 
-  const handleLogin = async (googleResponse) => {
-    try {
-      const idToken = googleResponse.getAuthResponse().id_token;
-
-      // Send the id_token to your backend
-      const userInfo = await sendIdTokenToBackend(idToken);
-      setProfile(userInfo);
-      sessionStorage.setItem('profile', JSON.stringify(userInfo));
-      navigate('/instruments');
-    } catch (error) {
-      console.error('Error during Google login:', error);
-    }
-  };
-
-  const sendIdTokenToBackend = async (idToken) => {
-    try {
-      const response = await axios.post(`${baseUrl}/auth`, { idToken });
-      return response.data;
-    } catch (error) {
-      console.error('Error sending id_token to backend:', error);
-      throw new Error('Failed to send id_token to backend');
-    }
-  };
-
+  // Effect to fetch user profile data on login
   useEffect(() => {
-    const fetchUserProfile = async () => {
-      try {
-        const response = await axios.get(`${baseUrl}/user/profile`);
-        setProfile(response.data);
-        sessionStorage.setItem('profile', JSON.stringify(response.data));
-      } catch (error) {
-        console.error('Error fetching user profile:', error);
-      }
-    };
-
-    fetchUserProfile();
+    const storedProfile = sessionStorage.getItem('profile');
+    if (storedProfile) {
+      setProfile(JSON.parse(storedProfile));
+    }
   }, []);
+
+  // Google login hook
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      // setUser(codeResponse);
+      axios
+        .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${codeResponse.access_token}`, {
+          headers: {
+            Authorization: `Bearer ${codeResponse.access_token}`,
+            Accept: 'application/json'
+          }
+        })
+        .then((res) => {
+          setProfile(res.data);
+          sessionStorage.setItem('profile', JSON.stringify(res.data));
+          navigate('/instruments');
+        })
+        .catch((err) => console.log(err));
+    },
+    onError: (error) => console.log('Login Failed:', error)
+  });
 
   return (
     <div>
       <NavigationBar baseUrl={baseUrl} profile={profile} logOut={handleLogout} />
       <Routes>
-        <Route path="/" element={<Home profile={profile} login={handleLogin} logout={handleLogout} />} />
+        <Route path="/" element={<Home profile={profile} login={handleLogin} logout={handleLogout}/>} />
         {profile && (
           <>
             <Route path="/instruments" element={<Instruments baseUrl={`${baseUrl}/instruments`} />} />
